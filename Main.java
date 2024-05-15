d77f384d275a636478e20e1f946ac380b1b2f309
Mandy <144916639+louisa-mandy@users.noreply.github.com>
2024-05-15 14:50:12 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,32 +5,44 @@ import java.util.HashMap;
 
 
 public class Main{
+
+    
     public static void main(String[] cupshot){
-        System.out.println("------------------  Welcome to CupShot!  ------------------");
-        System.out.println(" ");
-        System.out.println("1. Sign up for free voucher ( for first time purchase only ) \n2. Log in \n3. Continue as guest ");
 
-        System.out.println(" ");
-        System.out.println(" Choose your option: ");
-        System.out.println(" ");
+        while(true) {
 
-        Scanner sc = new Scanner(System.in); // creating the scanner, named "sc"
+            System.out.println("------------------  Welcome to CupShot!  ------------------");
+            System.out.println(" ");
+            System.out.println("1. Sign up for free voucher ( for first time purchase only ) \n2. Log in \n3. Continue as guest \n4. Exit Program");
 
-        Integer user_input = sc.nextInt(); // user input
+            System.out.println(" ");
+            System.out.println(" Choose your option: ");
+            System.out.println(" ");
+
+            Scanner sc = new Scanner(System.in); // creating the scanner, named "sc"
+
+            Integer user_input = sc.nextInt(); // user input
 
 
-        switch (user_input) {
-            case 1: // sign in
-                signin.handleSignIn();
-                
-                break;
+            switch (user_input) {
+                case 1: // sign in
+                    signin.handleSignIn();
+                    
+                    break;
 
-            case 2: // login with exsisting account 
-                login.handlelogin();
-                break;
+                case 2: // login with exsisting account 
+                    login.handlelogin();
+                    break;
 
-            case 3: // continue as a guest
-                break;
+                case 3: // continue as a guest
+                    guest.handleguest();
+                    break;
+
+                case 4: // exit program
+                    sc.close(); // Close the scanner 
+                    System.exit(0); // Exit the program
+                    break;
+            }
         }
     }
 }
